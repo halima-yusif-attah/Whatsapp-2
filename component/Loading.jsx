3b540932a7f9944826b5b6267bdce98d433120d6
Halima Yusif-Attah <halima.yusif-attah@amalitech.org>
2024-10-24 09:02:06 +0000

    fix: changed the directory of the whatsapp icon

--- component/Loading.jsx
+++ component/Loading.jsx
@@ -6,7 +6,7 @@ const Loading = () => {
     <center style={{display: 'grid', placeItems: 'center', height: '100vh'}}>
         <div>
             <Image 
-            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" 
+            src="/logo.png" 
             alt="whatsapp icon" 
             width={200} 
             height={200} 
